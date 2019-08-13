bf25b4a3eb24698f566a57567df4a6623b17e381
Mateusz Tyton <mateusz.tyton99@gmail.com>
2019-08-13 15:39:30 +0200

    Test problem solving, added Readme

--- shop/items/tests.py
+++ shop/items/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from django.urls import reverse
+import os
 
-# Create your tests here.
+
+class TestCategories(TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_data_no_auth(self):
+        url = reverse("categories")
+        response = self.client.get(url, format="json")
+        print(response)
--- shop/items/urls.py
+++ shop/items/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from .views import CategoriesView, ItemsView
 
 router = routers.DefaultRouter()
-router.register('categories', CategoriesView)
-router.register('items', ItemsView)
+router.register('categories', CategoriesView, basename="categories")
+router.register('items', ItemsView, basename="items")
 
 urlpatterns = [
     path('', include(router.urls)),
